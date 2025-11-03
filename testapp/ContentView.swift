98cb7bbddf2d7687821ee3e6ac86134b839fb760
なぎと-nagito- <hello@nagito.work>
2025-11-03 18:49:35 +0900

    Initial Commit

---
+++ testapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testapp
+//
+//  Created by 吉田 文太 on 2025/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by 吉田 文太 on 2025/11/03.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
